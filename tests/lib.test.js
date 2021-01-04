2e342543dea0243cf442689958fc4abd9e59fb0e
Rashad <rashad.walcott15@gmail.com>
2021-01-04 13:16:03 -0500

    Testing Exceptions

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -44,3 +44,19 @@ describe('getProduct', () => {
     expect(result).toHaveProperty('id', 1);
   });
 });
+
+describe('registerUser', () => {
+  it('should throw if username is falsy', () => {
+    const args = [null, undefined, NaN, '', 0, false];
+    args.forEach((a) => {
+      expect(() => {
+        lib.registerUser(a);
+      }).toThrow();
+    });
+  });
+  it('should return a user object if valid username is passed', () => {
+    const result = lib.registerUser('Rashad');
+    expect(result).toMatchObject({ username: 'Rashad' });
+    expect(result.id).toBeGreaterThan(0);
+  });
+});

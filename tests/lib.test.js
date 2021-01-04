5f57b02fd5b14b9a3788013300a89a298fb8cb89
Rashad <rashad.walcott15@gmail.com>
2021-01-04 11:54:21 -0500

    Testing Numbers

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -4,3 +4,13 @@ test('Our absolute - should return a positive number if input is positive', () =
   const result = lib.absolute(1);
   expect(result).toBe(1);
 });
+
+test('Our absolute - should return a positive number if input is negative', () => {
+  const result = lib.absolute(-1);
+  expect(result).toBe(1);
+});
+
+test('Our absolute - should return 0 if input is 0', () => {
+  const result = lib.absolute(0);
+  expect(result).toBe(0);
+});

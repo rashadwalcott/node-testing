8c6c4b2911b9ce79ed7e3c77fec9ffda75119442
Rashad <rashad.walcott15@gmail.com>
2021-01-04 12:56:04 -0500

    Testing Objects

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -33,3 +33,14 @@ describe('getCurrencies', () => {
     expect(result).toContain('USD', 'AUD', 'EUR');
   });
 });
+
+describe('getProduct', () => {
+  it('should return the product with the given id', () => {
+    const result = lib.getProduct(1);
+    // expect(result).toEqual({ id: 1, price: 10 });
+
+    expect(result).toMatchObject({ id: 1, price: 10 });
+
+    expect(result).toHaveProperty('id', 1);
+  });
+});
